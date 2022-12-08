c3bc9f50c538c9dbc7d020263abc9bf0cbcd476d
abhiram11 <abhiramsatpute@gmail.com>
2022-12-09 03:39:04 +0530

    added some comments for body parser as well

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv').config();
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 // Enable body parser
+//needed for POST and PUT requests to deal with the (incoming) data (object) in the body of the request.
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/openai', require('./routes/openaiRoutes'));
+app.use("/openai", require("./routes/openaiRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
